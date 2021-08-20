21c8061031f568700724e99008d2ce3a3db48708
Daniel Marin <danielmarin998@hotmail.com>
2021-08-20 12:55:46 -0500

    Metodos estaticos del prototipo Object

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CursoIntermedioDePOOenJS
+
+En este curso vamos a ir atras de el curso basico de JS, para profundizar como funcionan los objetos en JS.
---
+++ src/main.js
@@ -0,0 +1,21 @@
+const juan = {
+    name: "Juanito",
+    age:18,
+    approvedCourses: ["Curso1"],
+    addCourse(newCourse){
+        console.log("This", this);
+        console.log("This.approvedCourses", this.approvedCourses);
+        this.approvedCourses.push(newCourse);
+    }
+}
+
+// console.log(Object.keys(juan));
+// console.log(Object.getOwnPropertyNames(juan));
+// console.log(Object.entries(juan));
+console.log(Object.getOwnPropertyDescriptors(juan));
+Object.defineProperty(juan, "pruebaNASA", {
+    value: "Extraterrestres",
+    enumerable: true,
+    writable: true,
+    configurable: true,
+});
