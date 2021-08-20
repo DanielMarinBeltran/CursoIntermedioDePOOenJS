c4f14b2a5a8ab094e52cc3eb7983a17eb6cc6882
Daniel Marin <danielmarin998@hotmail.com>
2021-08-20 14:19:53 -0500

    metodos estaticos

--- src/main.js
+++ src/main.js
@@ -9,13 +9,39 @@ const juan = {
     }
 }
 
+Object.seal(juan);
+Object.freeze(juan);
+
 // console.log(Object.keys(juan));
 // console.log(Object.getOwnPropertyNames(juan));
 // console.log(Object.entries(juan));
+
+// Object.defineProperty(juan, "navigator", {
+//     value: "Chrome",
+//     enumerable: false,
+//     writable: true,
+//     configurable: false,
+// });
+
+// Object.defineProperty(juan, "editor", {
+//     value: "VSCode",
+//     enumerable: true,
+//     writable: false,
+//     configurable: true,
+// });
+
+// Object.defineProperty(juan, "terminal", {
+//     value: "WSL",
+//     enumerable: true,
+//     writable: true,
+//     configurable: false,
+// });
+
+// Object.defineProperty(juan, "pruebaNasa", {
+//     value: "Extraterrestres",
+//     enumerable: false,
+//     writable: false,
+//     configurable: false,
+// });
+
 console.log(Object.getOwnPropertyDescriptors(juan));
-Object.defineProperty(juan, "pruebaNASA", {
-    value: "Extraterrestres",
-    enumerable: true,
-    writable: true,
-    configurable: true,
-});
