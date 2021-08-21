b9690051199c086d06847709dcbe7e0f65908ad6
Daniel Marin <danielmarin998@hotmail.com>
2021-08-21 14:25:35 -0500

    inicio de duck typing

--- src/main.js
+++ src/main.js
@@ -59,6 +59,33 @@ function requiredParam(param){
     throw new Error(param + " es obligatorio.");
 };
 
+function createLearningPath({
+    name = requiredParam("name"),
+    courses = []
+}){
+    const private = {
+        "_name": name,
+        "_courses": courses,
+    };
+
+    const public = {
+        get name(){
+            return private["_name"];
+        },
+        set name(newName){
+            if (newName.length != 0){
+                private["_name"] = newName;
+            } else {
+                console.warn("Tu nombre debe tener al menos 1 caracter");
+            }
+        },
+        get courses(){
+            return private["_courses"];
+        },
+    };
+    return public;
+}
+
 function createStudent({
     name = requiredParam("name"),
     email = requiredParam("email"),
@@ -71,13 +98,13 @@ function createStudent({
 } = {} ){
     const private = {
         "_name": name,
+        "_learningPaths": learningPaths,
     };
 
     const public = {
         age,
         email,
         approvedCourses,
-        learningPaths,
         socialMedia: {
             twitter,
             instagram,
@@ -92,6 +119,25 @@ function createStudent({
             } else {
                 console.warn("Tu nombre debe tener al menos 1 caracter");
             }
+        },
+        get learningPaths(){
+            return private["_learningPaths"];
+        },
+        set learningPaths(newLP){
+            if (newLP.name){
+                console.warn("Tu LP no tiene courses");
+                return;
+            }
+            if (!newLP.courses){
+                console.warn("Tu LP no tiene la propiedad name");
+                return;
+            }
+            if (!isArray(newLP.courses)){
+                console.warn("Tu LP no es una lista (de cursos).");
+                return;
+            }
+            
+            private["_learningPaths"].push(newLP);
         },
 
 
